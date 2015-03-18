a1db111f65a9426f0d7bec59863301d27095676e
Ahmad Salim Al-Sibahi <asal@itu.dk>
2015-03-18 10:24:22 +0100

    Made the lexer

---
+++ src/FPromela/Lexer.hs
@@ -0,0 +1,91 @@
+module FPromela.Lexer where
+
+import qualified Text.Parsec.Token as Tok
+import Text.Parsec.Language (emptyDef)
+
+type Parser st a = Parsec String st a
+
+promelaDef :: Tok.LanguageDef st
+promelaDef = emptyDef {
+  Tok.commentStart   = "/*",
+  Tok.commentEnd     = "*/",
+  Tok.commentLine    = "//",
+  Tok.nestedComments = False,
+  Tok.reservedNames  = ["proctype", "init", "never",
+                        "trace", "mtype",
+                        "bit", "bool", "byte", "short", "int", "chan",
+                        "active", "priority", "provided", "hidden", "show",
+                        "unless", "xr", "xs",
+                        "eval", "if", "fi", "do", "od",
+                        "for", "atomic", "d_step",
+                        "select", "else", "break",
+                        "goto", "print", "assert",
+                        "c_code", "c_expr", "c_decl", "c_track", "c_state",
+                        "in", "len", "timeout",
+                        "np_", "enabled", "pc_value", "run",
+                        "full", "empty", "nfull", "nempty",
+                        "true", "false", "skip"]
+}
+
+lexer :: Tok.TokenParser st
+lexer = Tok.makeTokenParser promelaDef
+
+identifier :: Parser st String
+identifier = Tok.identifier lexer
+
+reserved :: Parser st String
+reserved = Tok.reserved lexer
+
+charLiteral :: Parser st Char
+charLiteral = Tok.charLiteral lexer
+
+stringLiteral :: Parser st String
+stringLiteral = Tok.stringLiteral lexer
+
+natural :: Parser st Integer
+natural = Tok.natural lexer
+
+symbol :: String -> Parser st String
+symbol = Tok.symbol lexer
+
+lexeme :: Parser st a -> Parser st a
+lexeme = Tok.lexeme lexer
+
+whiteSpace :: Parser st ()
+whiteSpace = Tok.whiteSpace lexer
+
+parens :: Parser st a -> Parser st a
+parens = Tok.parens lexer
+
+braces :: Parser st a -> Parser st a
+braces = Tok.braces lexer
+
+angles :: Parser st a -> Parser st a
+angles = Tok.angles lexer
+
+brackets :: Parser st a -> Parser st a
+brackets = Tok.brackets lexer
+
+semi :: Parser st String
+semi = Tok.semi lexer
+
+comma :: Parser st String
+comma = Tok.comma lexer
+
+colon :: Parser st String
+colon = Tok.colon lexer
+
+dot :: Parser st String
+dot = Token.dot lexer
+
+semiSep :: Parser st a -> Parser st [a]
+semiSep = Tok.semiSep lexer
+
+semiSep1 :: Parser st a -> Parser st [a]
+semiSep1 = Tok.semiSep1 lexer
+
+commaSep :: Parser st a -> Parser st [a]
+commaSep = Tok.commaSep lexer
+
+commaSep1 :: Parser st a -> Parser st [a]
+commaSep1 = Tok.commaSep1 lexer
